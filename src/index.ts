f10a3a13ed9c079e15a02384cff72ef1911dd1b8
SerGio <foresterares@gmail.com>
2023-12-25 04:54:05 +0200

    create button 'Del'

--- src/index.ts
+++ src/index.ts
@@ -4,38 +4,63 @@ interface ITodo {
 }
 
 class NoteBook {
-   private _todos: ITodo[]
+    private _todos: ITodo[]
 
 
-   constructor(private name:string) {
-       this._initMain()
-   }
+    constructor(private name:string) {
+        this._initMain()
+    }
 
 
-   private _getTodosFromLS(): void{
-       this._todos = JSON.parse(localStorage.getItem(this.name)) || [{id:1, title:'wait'}]
-   }
+    private _getTodosFromLS(): void{
+        this._todos = JSON.parse(localStorage.getItem(this.name)) || []
+    }
+
+    private _setTodoToFormLS():void{
+        localStorage.setItem(this.name, JSON.stringify(this._todos));
+        this._initTodos()
+    }
 
     private _initMain(): void{
-       this._initTodos()
+        this._initTodos();
+        this._initForm()
     }
 
 
     private _initTodos(): void {
-       this._getTodosFromLS();
-       const todosDiv = document.querySelector('#todos') as HTMLElement;
-       this._todos.forEach(todo=>{
-           const todoDiv = document.createElement('div');
-           todoDiv.innerText = `${todo.id}) ${todo.title}`;
-           todosDiv.append(todoDiv)
-       })
+        this._getTodosFromLS();
+        const todosDiv = document.querySelector('#todos') as HTMLElement;
+        todosDiv.innerHTML = '';
+        this._todos.forEach(todo=>{
+            const todoDiv = document.createElement('div');
+            todoDiv.innerText = `${todo.id}) ${todo.title}`;
+            const butDel = document.createElement('button');
+            butDel.innerText = 'Del';
+            butDel.onclick = function ():void{
+                todosDiv.removeChild(todoDiv);
+            }
+            todoDiv.append(butDel);
+            todosDiv.append(todoDiv)
+        })
     }
+
+
+
+    private _initForm(): void{
+        const form = document.forms['form'] as HTMLFormElement;
+        form.onsubmit = (e):void =>{
+            e.preventDefault();
+            const input = e.target['title'] as HTMLInputElement;
+            const  id: number = this._todos.slice(-1)[0]?.id + 1 || 1;
+            this._todos.push({id, title: input.value});
+            this._setTodoToFormLS();
+            form.reset()
+        }
+    }
+
+
+
 }
 
 
-
-
-
-
-
-// new NoteBook('wakeUp')
+new NoteBook( 'fds');
